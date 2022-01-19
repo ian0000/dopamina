bf273fe25de497b902134e9ba7d279f8ade347af
ian0000 <niklas0617@gmail.com>
2022-01-19 09:29:51 -0500

    Update actualizar.php

--- actualizar.php
+++ actualizar.php
@@ -16,11 +16,11 @@ var_dump($bucket);
 if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
     // FIXME: you should add more of your own validation here, e.g. using ext/fileinfo
     var_dump($_FILES);
+    $upload = $s3->upload($bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
+    var_dump($upload);
     try {
         // FIXME: you should not use 'name' for the upload, since that's the original filename from the user's computer - generate a random filename that you then store in your database, or similar
-        $upload = $s3->upload($bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
-        var_dump($upload);
-?>
+        ?>
         <p>Upload <a href="<?=htmlspecialchars($upload->get('ObjectURL'))?>">successful</a> :)</p>
 <?php } catch(Exception $e) { ?>
         <p>Upload error :(</p>
