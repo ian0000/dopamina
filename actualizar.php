74499825d6eb46bd3f6a055696b617704b593934
ian0000 <niklas0617@gmail.com>
2022-01-20 12:52:29 -0500

    Update actualizar.php

--- actualizar.php
+++ actualizar.php
@@ -1,14 +1,13 @@
 <?php
 require('vendor/autoload.php');
 // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
+use Aws\S3\S3Client; 
+use Aws\Exception\AwsException; 
+
 $s3 = new Aws\S3\S3Client([
     'version'  => 'latest',
     'region'   => 'us-east-2',
-    'credentials' =>
-    [
-		'key' => 'AWS_ACCESS_KEY_ID',
-		'secret' => 'AWS_SECRET_ACCESS_KEY'
-	]
+    
     
 ]);
 $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
@@ -19,21 +18,21 @@ var_dump($bucket);
     <body>
         <h1>S3 upload example</h1>
 <?php
-if($_SERVER['REQUEST_METHOD'] == 'POST' ) {
-
-    if(isset($_FILES['file'])){
-        $uploadObject = $s3->putObject(
-		[
-			'Bucket' => 's3-demo-dopa',
-			'Key' => $_FILES['file']['name'],
-			'SourceFile' => $_FILES['file']['tmp_name']
-		]); 
-    }
-    var_dump($uploadObject);
-} ?>
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
+    // FIXME: you should add more of your own validation here, e.g. using ext/fileinfo
+    var_dump($_FILES);
+    try {
+        // FIXME: you should not use 'name' for the upload, since that's the original filename from the user's computer - generate a random filename that you then store in your database, or similar
+        $upload = $s3->upload($bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
+        var_dump($upload);
+        ?>
+        <p>Upload <a href="<?=htmlspecialchars($upload->get('ObjectURL'))?>">successful</a> :)</p>
+<?php } catch(Exception $e) { echo $e ?>
+        <p>Upload error :(</p>
+<?php } } ?>
         <h2>Upload a file</h2>
         <form enctype="multipart/form-data" action="<?=$_SERVER['PHP_SELF']?>" method="POST">
-            <input name="file" type="file"><input type="submit" value="Upload">
+            <input name="userfile" type="file"><input type="submit" value="Upload">
         </form>
     </body>
 </html>
