c5836459528ff7a08321366fffbd4325ffd2f7cc
ian0000 <niklas0617@gmail.com>
2022-01-21 23:16:17 -0500

    Update ztest.php

--- ztest.php
+++ ztest.php
@@ -5,29 +5,23 @@ use PHPMailer\PHPMailer\Exception;
 
 require 'vendor/autoload.php';
 $mail = new PHPMailer();
-try{
-    
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'ianmenaamagua@gmail.com'; // Gmail address which you want to use as SMTP server
-    $mail->Password = 'Nikiui17'; // Gmail address Password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port = '587';
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->Username = 'ianmenaamagua@gmail.com'; // Gmail address which you want to use as SMTP server
+$mail->Password = 'Nikiui17'; // Gmail address Password
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = '587';
 
-    $mail->setFrom('ianmenaamagua@gmail.com'); // Gmail address which you used as SMTP server
-    $mail->addAddress('niklas0617@gmail.com'); // Email address where you want to receive emails (you can use any of your gmail address including the gmail address which you used as SMTP server)
+$mail->setFrom('ianmenaamagua@gmail.com'); // Gmail address which you used as SMTP server
+$mail->addAddress('niklas0617@gmail.com'); // Email address where you want to receive emails (you can use any of your gmail address including the gmail address which you used as SMTP server)
 
-    $mail->isHTML(true);
-    $mail->Subject = 'Message Received (Contact Page)';
-    $mail->Body = "<h3>Name : ian <br>Email: l <br>Message : message</h3>";
+$mail->isHTML(true);
+$mail->Subject = 'Message Received (Contact Page)';
+$mail->Body = "<h3>Name : ian <br>Email: l <br>Message : message</h3>";
 
-    $mail->send();
-    $alert = '<div class="alert-success">
-                 <span>Message Sent! Thank you for contacting us.</span>
-                </div>';
-  } catch (Exception $e){
-    $alert = '<div class="alert-error">
-                <span>'.$e->getMessage().'</span>
-              </div>';
-  }
+if ($mail->send()) {
+    echo "yey";
+}else{
+    echo "ney";
+}
