87037d62268d3abc136b2d602f2ed6382076eb0f
ian0000 <niklas0617@gmail.com>
2022-01-22 09:47:17 -0500

    Update contacto.php

--- contacto.php
+++ contacto.php
@@ -1,11 +1,10 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-require 'includes/funciones.php';
-require('vendor/autoload.php');
+require '../../includes/funciones.php';
+require '../../includes/config/database.php';
 $errores= [];
 // variables del formulario
+
+$db = conectarDB();
 $nombre = '';
 $correo = '';
 $celular = '';
@@ -28,30 +27,11 @@ if($_SERVER['REQUEST_METHOD']==='POST'){
         $errores[] = 'Debes agregar un mensaje';
     }
     if(empty($errores)){
-        $emailuser = getenv('EMAILUSERNAME');
-        $emailpass = getenv('EMAILPASSWORD');
-        $emailhost = getenv("EMAILHOST");
         
-
-        $mensajefinal = "correo:".$correo."\n"."nombre".$nombre."\n"."celular".$celular."\n"."mensaje".$mensaje;
-        $mail = new PHPMailer();
-        $mail->isSMTP();
-        $mail->Host = $emailhost;
-        $mail->SMTPAuth = "true";
-        $mail->SMTPSecure = "tls";
-        $mail->Port = "587";
-        $mail->Username = $emailuser;
-        $mail->Password = $emailpass;
-        $mail->Subject = "Test Email";
-        $mail->setFrom($emailuser);
-        $mail->Body = "this is a test";
-        $mail->addAddress("niklas0617@gmail.com");
-        if ($mail->Send()) {
-            echo "mail sent";
-        } else{
-            echo "error".$mail->ErrorInfo;
-            echo "asass";
-            echo $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+        $query = "INSERT INTO contacto(nombre, correo, celular, mensaje) VALUES ('$nombre','$correo', '$celular', '$mensaje');";
+        $resultado - mysqli_query($db, $query);
+        if($resultado){
+            header('Location: contacto.php');
         }
     }
 }
@@ -66,7 +46,7 @@ incluirTemplate('header');
     <fieldset>
         <legend>Envianos un Mensaje</legend>
         <label for="nombre">Nombre y Apellido</label>
-        <input type="text" class="dato" name="nombre" placeholder="Tu Nombre aquí....">
+        <input type="text" class="dato" name="nombre" placeholder="Tu Nombre aquí...." required>
         <label for="correo">Correo de Contacto<span>*</span></label>
         <input type="email" class="dato" name="correo" placeholder="Tu Correo aquí...." required>
         <label for="celular">Tu Número de Celular <span>*</span></label>
