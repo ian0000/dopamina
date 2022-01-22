6f71926b26575e77d0c041fc4e7d36df4c376fc4
ian0000 <niklas0617@gmail.com>
2022-01-21 22:46:48 -0500

    Update ztest.php

--- ztest.php
+++ ztest.php
@@ -1,9 +1,27 @@
 <?php
-require('vendor/autoload.php');
-$my_env_var = getenv('EMAILUSERNAME');
-echo $my_env_var;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-$my_env_var = getenv('EMAILPASSWORD');
-echo $my_env_var;
-
-//ESTO ES PARA OBTENER LAS CLAVES DESDE HEROKU
+require 'vendor/autoload.php';
+$emailuser = "ianmenaamagua@gmail.com";
+$emailpass = "Nikiui17";
+$emailhost = "Smtp.gmail.com";
+var_dump($emailhost);
+$mail = new PHPMailer();
+$mail->isSMTP();
+$mail->Host = $emailhost;
+$mail->SMTPAuth = "true";
+$mail->SMTPSecure = "tls";
+$mail->Port = "587";
+$mail->Username = $emailuser;
+$mail->Password = $emailpass;
+$mail->Subject = "Test Email";
+$mail->setFrom($emailuser);
+$mail->Body = "this is a test";
+$mail->addAddress("niklas0617@gmail.com");
+if ($mail->Send()) {
+	echo "mail sent";
+} else{
+	echo "error";
+}
